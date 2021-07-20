26695f93356da01002ed81811e7d60898ed79356
Michno Marcin <Marcin.Michno@exorigo-upos.pl>
2021-07-20 14:54:23 +0200

    deployment

--- src/components/car/CarModel.js
+++ src/components/car/CarModel.js
@@ -1,13 +1,17 @@
 import React, {useRef} from 'react'
 import {useSelector} from 'react-redux';
-import { useGLTF} from '@react-three/drei'
+import { useGLTF} from '@react-three/drei';
+import car1 from '../../resources/carModels/car1.gltf'
+import car2 from '../../resources/carModels/car2.gltf'
+import car3 from '../../resources/carModels/car3.gltf'
+import car4 from '../../resources/carModels/car4.gltf'
 
 export const CarModel = (props) => {
     const group = useRef()
     const currentCar = useSelector(state => state.changeCarReducer);
     const currentColor = useSelector(state => state.selectCarColorReducer);
     
-    const { nodes, materials } = useGLTF(`/resources/carModels/car${currentCar.id}.gltf`);
+    const { nodes, materials } = useGLTF(`car${currentCar.id}`);
     
     return(
     <group ref={group} {...props} dispose={null} position={[0, -1.1, 0]}>
